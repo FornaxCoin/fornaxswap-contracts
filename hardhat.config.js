1c196255250373c5665e5daae7ef00e8307fa78c
qasim raheem 🚀 <qasimmehmood13936@gmail.com>
2022-07-25 15:28:52 +0500

    first commit

---
+++ hardhat.config.js
@@ -0,0 +1,68 @@
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+
+require('@nomiclabs/hardhat-ethers');
+
+// Change private keys accordingly - ONLY FOR DEMOSTRATION PURPOSES - PLEASE STORE PRIVATE KEYS IN A SAFE PLACE
+// Export your private key as
+//       export PRIVKEY=0x.....
+const privateKey = process.env.PRIVKEY;
+const privateKeyDev =
+   '6343d2e0502e5133cb74c528189598264456fdd079c72cbaa78d3605defc3801';
+
+module.exports = {
+   defaultNetwork: 'hardhat',
+
+   networks: {
+      hardhat: {},
+
+      // moonbase: {
+      //    url: 'https://rpc.api.moonbase.moonbeam.network',
+      //    accounts: [privateKey],
+      //    chainId: 1287,
+      // },
+      // dev: {
+      //    url: 'http://127.0.0.1:9933',
+      //    accounts: [privateKeyDev],
+      //    network_id: '1281',
+      //    chainId: 1281,
+      // },
+      fornax: {
+         url: 'https://testnetv1.watchfornax.com/rpc',
+         accounts: [privateKeyDev],
+         network_id: '13937',
+         chainId: 13937,
+      },
+   },
+   solidity: {
+      compilers: [
+         {
+            version: '0.5.16',
+            settings: {
+               optimizer: {
+                  enabled: true,
+                  runs: 200,
+               },
+            },
+         },
+         {
+            version: '0.6.6',
+            settings: {
+               optimizer: {
+                  enabled: true,
+                  runs: 200,
+               },
+            },
+         },
+      ],
+   },
+   paths: {
+      sources: './contracts',
+      cache: './cache',
+      artifacts: './artifacts',
+   },
+   mocha: {
+      timeout: 20000,
+   },
+};
---
+++ scripts/deploy-factory.js
@@ -0,0 +1,45 @@
+const { ethers } = require('hardhat');
+
+// Deploy function
+async function deploy() {
+   [account] = await ethers.getSigners();
+   deployerAddress = account.address;
+   console.log(`Deploying contracts using ${deployerAddress}`);
+
+   //Deploy WETH
+   const weth = await ethers.getContractFactory('WETH');
+   const wethInstance = await weth.deploy();
+   await wethInstance.deployed();
+
+   console.log(`WETH deployed to : ${wethInstance.address}`);
+
+   //Deploy Factory
+   const factory = await ethers.getContractFactory('UniswapV2Factory');
+   const factoryInstance = await factory.deploy(deployerAddress);
+   await factoryInstance.deployed();
+
+   console.log(`Factory deployed to : ${factoryInstance.address}`);
+
+   //Deploy Router passing Factory Address and WETH Address
+   const router = await ethers.getContractFactory('UniswapV2Router02');
+   const routerInstance = await router.deploy(
+      '0xd4089F4057227462c8AF6129fC1a279e3f634AD6','0x83E87145Ac92037Eae9035988f5A8599228f150f'
+   );
+   await routerInstance.deployed();
+
+   console.log(`Router V02 deployed to :  ${routerInstance.address}`);
+
+   //Deploy Multicall (needed for Interface)
+   const multicall = await ethers.getContractFactory('Multicall');
+   const multicallInstance = await multicall.deploy();
+   await multicallInstance.deployed();
+
+   console.log(`Multicall deployed to : ${multicallInstance.address}`);
+}
+
+deploy()
+   .then(() => process.exit(0))
+   .catch((error) => {
+      console.error(error);
+      process.exit(1);
+   });
